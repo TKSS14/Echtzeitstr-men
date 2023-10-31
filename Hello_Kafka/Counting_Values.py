e002fccd9e71220cc6697b448699e7e6af632cc5
IlINNIlI <128753303+IlINNIlI@users.noreply.github.com>
2023-10-31 16:52:28 +0100

    Add files via upload

--- Hello_Kafka/Counting_Values.py
+++ Hello_Kafka/Counting_Values.py
@@ -10,24 +10,12 @@ app = faust.App('Counting_Values', broker='kafka://localhost:9092')
 topic = app.topic('temp', value_type=Sensor)
 
 
-@app.agent(topic)
-async def signal(temperature):
-    async for temp in temperature:
-        print(f'Temperature: {temp.temp_val}')
-
-
 @app.timer(interval=1.0)
 async def example_sender(app):
     temperatur = rnd.uniform(20, 30)
-    await signal.send(
+    await topic.send(
         value=Sensor(temp_val=temperatur),
     )
-
-
-@app.agent(topic)
-async def count(temperature):
-    async for i, value in temperature.enumerate():
-        print(f'Eventzähler: {i}')
 
 
 app.main()
--- Hello_Kafka/monitoring.py
+++ Hello_Kafka/monitoring.py
@@ -8,25 +8,26 @@ class Sensor(faust.Record):
 app = faust.App('Counting_Values', broker='kafka://localhost:9092')
 topic = app.topic('temp', value_type=Sensor)
 
-"""
-@app.agent(app.topic('temp'))
+
+@app.agent(topic)
 async def mytask(events):
     async for event in events:
-        print(f'test {event}')
-        #print(f'Events pro s: {app.monitor.events_s}')
-        #print(f'Events gesamt: {app.monitor.events_total}')
+        print(f'Events pro s: {app.monitor.events_s}')
+        print(f'Events gesamt: {app.monitor.events_total}')
+        print(f'Nachrichten zum Topic gesamt: {app.monitor.messages_received_by_topic}')
+        print(f'Events gesamt(Offset): {app.monitor.tp_read_offsets.values()}')
 
-"""
+
 @app.agent(topic)
 async def count(temperature):
-    async for i, value in temperature.enumerate():
+    async for i, value in temperature.enumerate(start=1):
         print(f'Eventzähler: {i}')
+
 
 @app.agent(topic)
 async def signal(temperature):
     async for temp in temperature:
         print(f'Temperature: {temp.temp_val}')
-
 
 
 app.main()
